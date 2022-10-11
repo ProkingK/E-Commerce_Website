df484043d236a861a2fcae06f66ff1ab18726e28
Ashley Kapaso <ashleykapaso@gmail.com>
2022-10-11 20:21:12 +0200

    First commit

---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Views for rendering templates
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
